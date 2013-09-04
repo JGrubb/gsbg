f8c497acfaa5c9e3e23304a1e07c0eda542ddadf
John Grubb <therealjohnnygrubb@gmail.com>
2013-09-04 16:40:36 -0400

    Begin tests, no really

--- test/controllers/shows_controller_test.rb
+++ test/controllers/shows_controller_test.rb
@@ -16,23 +16,8 @@ class ShowsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
   test "should get edit" do
     get :edit
-    assert_response :success
-  end
-
-  test "should get update" do
-    get :update
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get :destroy
     assert_response :success
   end
 
