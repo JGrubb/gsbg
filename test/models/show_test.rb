becbbc88e09fbaa21d68789e6246f06a2ff6cf72
John Grubb <therealjohnnygrubb@gmail.com>
2013-09-06 20:56:06 -0400

    Show unit tests

--- test/models/show_test.rb
+++ test/models/show_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class ShowTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert false
+  test "should not save without venue or date" do
+    show = Show.new
+    assert !show.save, "Saved show without venue or date"
   end
+
+  test "should not save without date" do
+    show = Show.new
+    show.venue = "The Fillmore"
+    assert !show.save, "Saved show with date"
+  end
+
+  test "should not save without venue" do
+    show = Show.new
+    show.date = "2013-12-12"
+    assert !show.save, "Saved show without venue"
+  end
+
+  test "should save show" do
+    show = Show.new
+    show.date = "2013-12-12"
+    show.venue = "The fillmore"
+    assert show.save, "Did not save show with proper fields"
+  end
+
 end
