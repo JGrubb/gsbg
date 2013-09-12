f93acb634e9aa8960974a73877bac76bc94086d8
John Grubb <therealjohnnygrubb@gmail.com>
2013-09-12 20:57:53 +0000

    Top bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Gsbg::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { 
+                      :sign_in => 'login',
+                      :sign_out => 'logout',
+                      :sign_up => 'register' 
+  }
   resources :shows
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
